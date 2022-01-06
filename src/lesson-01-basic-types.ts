52820491677639d53e436b7d41df7d5d0fbfc3df
zsofip <91950899+zsofip@users.noreply.github.com>
2022-01-06 22:08:04 +0100

    done

--- src/lesson-01-basic-types.ts
+++ src/lesson-01-basic-types.ts
@@ -4,24 +4,25 @@ export default class calculator {
 
     // Hozd létre a következő változókat a megfelelő típusannotációval.
     /** @type {number} members - value: 11 */
-    
+    members: number = 11;
 
     /** @type {number} baseColor - value: a3f4d5 (hexadecimal number) */
-    
+    baseColor: number = parseInt('a3f4d5', 16);
 
     /** @type {number} role - value: 1001 (binary number) */
-    
+    role: number = parseInt('1001', 2);
 
     /** @type {number} systemID - value: 7700 (octal number) */
-    
+    systemID: number = parseInt('7700', 8);
 
     /** @type {number} average - value: 4.5 */
-    
+    average: number = 4.5;
 
     /** @type {string} adminName - value: szabadon választott szöveg */
-    
+    adminName: string = 'Amy Namey';
 
     /** @type {string} sumPoints - value: 'Members have <this.members és a this.average szorzata> points' (template string) */
+    sumPoints: string = `Members have ${this.members*this.average} points`;
 
 
 }
